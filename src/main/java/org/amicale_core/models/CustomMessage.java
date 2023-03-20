49ca78964280080b2410c5c00414b865cd8b1dd4
Czekaj Tom <red57101@gmail.com>
2023-03-20 11:20:57 +0100

    :memo: Add documentation for CustomMessage

--- src/main/java/org/amicale_core/models/CustomMessage.java
+++ src/main/java/org/amicale_core/models/CustomMessage.java
@@ -3,7 +3,16 @@ package org.amicale_core.models;
 import net.dv8tion.jda.api.entities.Message;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Class used to sort best wanted suggestions
+ * @param message The {@link Message}
+ * @param reactionCount The number of positive reactions
+ */
 public record CustomMessage(@NotNull Message message, int reactionCount) {
+    /**
+     * Get the product name from the {@link Message}
+     * @return The product name as a {@link String}
+     */
     public String getProductName() {
         return message.getEmbeds().get(0).getTitle();
     }
