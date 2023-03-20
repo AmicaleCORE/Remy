c76f23a882562cec2eb7be262d2b98051d544792
Czekaj Tom <red57101@gmail.com>
2023-03-20 11:23:54 +0100

    :memo: Add documentation for SuggestCommand

--- src/main/java/org/amicale_core/commands/SuggestCommand.java
+++ src/main/java/org/amicale_core/commands/SuggestCommand.java
@@ -13,8 +13,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+/**
+ * Class used to create the /suggest command
+ */
 public class SuggestCommand extends AbstractSlashCommand {
 
+    /**
+     * Default constructor for tge suggest command
+     * Create the command using {@link SlashCommandOption}
+     */
     public SuggestCommand() {
         super("suggest", "Allow you to suggest a product",
                 new SlashCommandOption(OptionType.STRING, "product_name", "The name of the product", false));
