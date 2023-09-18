c9be482920ff547eeddcff017f8abe9c5cd214ba
Czekaj Tom <red57101@gmail.com>
2023-09-18 13:42:12 +0200

    :sparkles: Add music suggestion embed

--- src/main/java/org/amicale_core/models/embeds/MusicSuggestionEmbed.java
+++ src/main/java/org/amicale_core/models/embeds/MusicSuggestionEmbed.java
@@ -3,22 +3,34 @@ package org.amicale_core.models.embeds;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import org.amicale_core.Remy;
+import org.amicale_core.models.YoutubeResponse;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
 
 /**
  * Class used to display the suggestion when the /suggest command is executed
  */
 public class MusicSuggestionEmbed extends EmbedBuilder {
-    /**
-     * Constructor of the Suggestion Embed class
-     * @param member {@link Member} who send the suggestion
-     */
-    public MusicSuggestionEmbed(@NotNull final String musicName, @NotNull final Member member){
-        this.setTitle(musicName.toUpperCase());
-        this.setDescription(String.format("Suggestion de %s", member.getAsMention()));
+    public MusicSuggestionEmbed(@NotNull final String musicLick, @NotNull final String memberMention){
+        System.out.println(getVideoId(musicLick));
+        YoutubeResponse response = YoutubeResponse.getYoutubeVideoInfos(getVideoId(musicLick));
+        if(Objects.isNull(response)) return;
+        this.setTitle(response.title());
+        this.setImage(response.thumbnail());
+        this.setUrl(musicLick);
+        this.setDescription(String.format("Suggestion de %s", memberMention));
         this.setFooter(Remy.CONFIG.getEmbedFooter());
         this.setColor(Color.ORANGE);
+    }
+
+    private String getVideoId(String musicLink){
+        return musicLink.split("v=")[1].split("&")[0];
     }
 }
