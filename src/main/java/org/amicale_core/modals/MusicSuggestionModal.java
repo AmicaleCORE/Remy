1d5fe0dc97450d705d73d9db3ffa49db4b57b38b
Czekaj Tom <red57101@gmail.com>
2023-09-18 13:43:29 +0200

    :bug: Remove unuse modal field and fix minor bugs

--- src/main/java/org/amicale_core/modals/MusicSuggestionModal.java
+++ src/main/java/org/amicale_core/modals/MusicSuggestionModal.java
@@ -17,24 +17,22 @@ import java.util.Objects;
 
 public class MusicSuggestionModal extends AbstractModal {
 
-    private static final ModalField musicNameField = new ModalField("music_name", "Nom de la musique", TextInputStyle.SHORT, "Nom de la musique", 0, 50);
     private static final ModalField musicLinkField = new ModalField("music_link", "Lien youtube de la musique", TextInputStyle.SHORT, "Lien youtube de la musique", 0, 100);
 
     public MusicSuggestionModal() {
-        super("music_suggestion", "Suggestion de musique", musicNameField, musicLinkField);
+        super("music_suggestion", "Suggestion de musique", musicLinkField);
     }
 
     @Override
     public void callback(@NotNull DJApp djApp, @NotNull ModalInteractionEvent e) {
-        ModalMapping musicName = e.getValue("music_name");
         ModalMapping musicLink = e.getValue("music_link");
-        if(Objects.isNull(musicName) || Objects.isNull(musicLink)){
+        if(Objects.isNull(musicLink)){
             ProductSuggestionModal modal = new ProductSuggestionModal();
             djApp.getComponentsManager().handleModal(e.getUser().getIdLong(), modal);
-            e.reply("Can't find music name or link").setEphemeral(true).queue();
+            e.reply("Can't find music link").setEphemeral(true).queue();
             return;
         }
-        if(!musicLink.getAsString().contains("youtube.com/")){
+        if(!musicLink.getAsString().contains("youtube.com/") && !musicLink.getAsString().contains("youtu.be/")){
             e.reply("The link must be a youtube link").setEphemeral(true).queue();
             return;
         }
@@ -43,7 +41,7 @@ public class MusicSuggestionModal extends AbstractModal {
             e.reply("Can't find member").setEphemeral(true).queue();
             return;
         }
-        MessageEmbed embed = new MusicSuggestionEmbed(musicName.getAsString(), member).build();
-        Remy.sendSuggestion(musicLink.getAsString(), embed, e, SuggestionType.MUSIC);
+        MessageEmbed embed = new MusicSuggestionEmbed(musicLink.getAsString(), member.getAsMention()).build();
+        Remy.sendSuggestion(null, embed, e, SuggestionType.MUSIC);
     }
 }
